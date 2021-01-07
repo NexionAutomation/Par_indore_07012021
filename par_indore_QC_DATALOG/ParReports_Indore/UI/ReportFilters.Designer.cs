43d6a5986db8da8b2722150f402de1da5d8a6943
daeepak baghel <deepakbaghel511@gmail.com>
2021-01-07 12:22:18 +0000

    test

---
+++ par_indore_QC_DATALOG/ParReports_Indore/UI/ReportFilters.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace ParReports_Indore
+{
+	partial class ReportFilters
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbLoad = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbReportTypes = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtEnd = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtStart = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbExport = new System.Windows.Forms.GroupBox();
+            this.btnPDF = new System.Windows.Forms.Button();
+            this.btnExportToExcel = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.gbExport.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbLoad);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.cmbReportTypes);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.dtEnd);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.dtStart);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(790, 71);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Report Filters";
+            // 
+            // cmbLoad
+            // 
+            this.cmbLoad.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbLoad.FormattingEnabled = true;
+            this.cmbLoad.Location = new System.Drawing.Point(631, 32);
+            this.cmbLoad.Name = "cmbLoad";
+            this.cmbLoad.Size = new System.Drawing.Size(131, 23);
+            this.cmbLoad.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(648, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Time Span";
+            // 
+            // cmbReportTypes
+            // 
+            this.cmbReportTypes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbReportTypes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbReportTypes.FormattingEnabled = true;
+            this.cmbReportTypes.Location = new System.Drawing.Point(400, 33);
+            this.cmbReportTypes.Name = "cmbReportTypes";
+            this.cmbReportTypes.Size = new System.Drawing.Size(200, 23);
+            this.cmbReportTypes.TabIndex = 0;
+            this.cmbReportTypes.SelectedIndexChanged += new System.EventHandler(this.cmbReportTypes_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(424, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Report Types";
+            // 
+            // dtEnd
+            // 
+            this.dtEnd.CustomFormat = "dd/MM/yyyy HH:mm:ss";
+            this.dtEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtEnd.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtEnd.Location = new System.Drawing.Point(204, 35);
+            this.dtEnd.MinDate = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            this.dtEnd.Name = "dtEnd";
+            this.dtEnd.Size = new System.Drawing.Size(174, 21);
+            this.dtEnd.TabIndex = 3;
+            this.dtEnd.ValueChanged += new System.EventHandler(this.dtEnd_ValueChanged);
+            this.dtEnd.Leave += new System.EventHandler(this.dtEnd_Leave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(270, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "To";
+            // 
+            // dtStart
+            // 
+            this.dtStart.CustomFormat = "dd/MM/yyyy HH:mm:ss";
+            this.dtStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtStart.Location = new System.Drawing.Point(11, 35);
+            this.dtStart.MinDate = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            this.dtStart.Name = "dtStart";
+            this.dtStart.Size = new System.Drawing.Size(176, 21);
+            this.dtStart.TabIndex = 2;
+            this.dtStart.ValueChanged += new System.EventHandler(this.dtDate_ValueChanged);
+            this.dtStart.Leave += new System.EventHandler(this.dtStart_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From";
+            // 
+            // gbExport
+            // 
+            this.gbExport.Controls.Add(this.btnPDF);
+            this.gbExport.Controls.Add(this.btnExportToExcel);
+            this.gbExport.Controls.Add(this.btnPrint);
+            this.gbExport.Controls.Add(this.btnShow);
+            this.gbExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbExport.Location = new System.Drawing.Point(798, 3);
+            this.gbExport.Name = "gbExport";
+            this.gbExport.Size = new System.Drawing.Size(221, 68);
+            this.gbExport.TabIndex = 15;
+            this.gbExport.TabStop = false;
+            // 
+            // btnPDF
+            // 
+            this.btnPDF.BackgroundImage = global::ParReports_Indore.Properties.Resources.pdf_icon_png_2056;
+            this.btnPDF.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPDF.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPDF.Location = new System.Drawing.Point(79, 11);
+            this.btnPDF.Margin = new System.Windows.Forms.Padding(0);
+            this.btnPDF.Name = "btnPDF";
+            this.btnPDF.Size = new System.Drawing.Size(50, 50);
+            this.btnPDF.TabIndex = 4;
+            this.btnPDF.UseVisualStyleBackColor = true;
+            this.btnPDF.Click += new System.EventHandler(this.btnPDF_Click);
+            // 
+            // btnExportToExcel
+            // 
+            this.btnExportToExcel.BackgroundImage = global::ParReports_Indore.Properties.Resources.excel_xls_icon;
+            this.btnExportToExcel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnExportToExcel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExportToExcel.Location = new System.Drawing.Point(221, 15);
+            this.btnExportToExcel.Name = "btnExportToExcel";
+            this.btnExportToExcel.Size = new System.Drawing.Size(60, 50);
+            this.btnExportToExcel.TabIndex = 3;
+            this.btnExportToExcel.UseVisualStyleBackColor = true;
+            this.btnExportToExcel.Click += new System.EventHandler(this.btnExportToExcel_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.BackgroundImage = global::ParReports_Indore.Properties.Resources.print;
+            this.btnPrint.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPrint.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrint.Location = new System.Drawing.Point(73, 11);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(60, 50);
+            this.btnPrint.TabIndex = 2;
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Visible = false;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.BackgroundImage = global::ParReports_Indore.Properties.Resources.call_report_icon_3;
+            this.btnShow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShow.Location = new System.Drawing.Point(15, 13);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(53, 49);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // ReportFilters
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.gbExport);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ReportFilters";
+            this.Size = new System.Drawing.Size(1100, 76);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.gbExport.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.DateTimePicker dtStart;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox gbExport;
+		private System.Windows.Forms.Button btnShow;
+		private System.Windows.Forms.Button btnPrint;
+		private System.Windows.Forms.ComboBox cmbReportTypes;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.DateTimePicker dtEnd;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ComboBox cmbLoad;
+		private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnExportToExcel;
+        private System.Windows.Forms.Button btnPDF;
+    }
+}
